a93a556247c24be2bfe7f640f402505e61129177
Alexander Sudo <alexanderurchenco.ru@gmail.com>
2024-01-27 22:42:12 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# postirony!
+# Postirony!
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
